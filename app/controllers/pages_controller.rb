12e85b62abf0bad39136fd3bf06dd746167a4090
Petr Janik <485122@mail.muni.cz>
2020-10-05 16:54:08 +0200

    Finish about and contact page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,4 @@
 class PagesController < ApplicationController
-  def welcome
-
-  end
-
   def portfolio
     @projects = Project.order(created_at: :asc)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
-
   get '/portfolio' => 'pages#portfolio'
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
 end
